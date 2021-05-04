3d909800f6846bc05216f0456a8abf01891d88db
izadori <blueocean.okinawa@gmail.com>
2021-05-05 00:38:29 +0900

    [add]first commit

---
+++ src/js/client.js
@@ -0,0 +1,166 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const parseFormula = require('./parseformula.js');
+const pt = require('./element_table');
+
+class InputField extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.props.onFieldChange(e.target.value);
+  }
+
+  handleSubmit(e) {
+    this.props.onFieldSubmit(this.props.formula);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <form id="chemical-formula-form">
+          <span id="form-title">化学式</span>
+          <input type="text" id="form-input" value={this.props.formula} onChange={this.handleChange}/>
+          <button type="button" id="form-button" onClick={this.handleSubmit}>計算</button>
+          <span id="form-status-window">{this.props.status}</span>
+        </form>
+      </div>
+    );
+  }
+}
+
+function FormulaWeight(props) {
+  return (
+    <p id="fw">式量 = <span id="fw-value">{parseInt(props.fw * 1000) / 1000}</span></p>
+  );
+}
+
+class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formula: '',
+      atoms: new Map(),
+      ratio: new Map(),
+      fw: 0.0,
+      status: '',
+    };
+    this.handleFieldChange = this.handleFieldChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.renderTableData = this.renderTableData.bind(this);
+    this.renderAllTableData = this.renderAllTableData.bind(this);
+  }
+
+  handleFieldChange(formula) {
+    this.setState({ formula: formula });
+  }
+
+  handleSubmit() {
+    const atoms = parseFormula(this.state.formula);
+    const ratio = new Map();
+    let fw = 0.0;
+    let atomCount = 0;
+
+    if(atoms == null) {
+      this.setState({
+        atoms: new Map(),
+        ratio: new Map(),
+        fw: 0.0,
+        status: '化学式が正しくありません'
+      });
+      return;
+    }
+
+    for(let symbol of atoms.keys()) {
+      let z = pt.periodicTable.symbolToNo(symbol);
+      atomCount += atoms.get(symbol);
+      fw += pt.periodicTable.getWeight(z) * atoms.get(symbol);
+    }
+
+    for(let symbol of atoms.keys()) {
+      let z = pt.periodicTable.symbolToNo(symbol);
+      ratio.set(symbol, {
+        at: parseFloat(atoms.get(symbol)) / parseFloat(atomCount),
+        wt: pt.periodicTable.getWeight(z) / fw
+      });
+    }
+
+    this.setState({
+      atoms: atoms,
+      ratio: ratio,
+      fw: fw,
+      status: ''
+    });
+  }
+
+  renderTableData(symbol) {
+    const at = this.state.ratio.get(symbol).at;
+    const wt = this.state.ratio.get(symbol).wt;
+    const z = pt.periodicTable.symbolToNo(symbol);
+    return (
+      <tr key={z}>
+        <td className="table-data data-label">{z}</td>
+        <td className="table-data data-label">{symbol}</td>
+        <td className="table-data data-value">{this.state.atoms.get(symbol)}</td>
+        <td className="table-data data-value">{pt.periodicTable.getWeight(z)}</td>
+        <td className="table-data data-value">{(typeof at !== 'undefined') ? (parseInt(at * 10000) / 100).toString() : '---'}</td>
+        <td className="table-data data-value">{(typeof wt !== 'undefined') ? (parseInt(wt * 10000) / 100).toString() : '---'}</td>
+      </tr>
+    )
+  }
+
+  renderAllTableData() {
+    let data = [];
+    let keys = [];
+    for(let symbol of this.state.atoms.keys()){
+      keys.push(symbol);
+    }
+    for(let symbol of keys.sort(
+      (a, b) => pt.periodicTable.symbolToNo(a) - pt.periodicTable.symbolToNo(b))
+    ) {
+      data.push(this.renderTableData(symbol));
+    }
+    return(<>{data}</>);
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+    <div>
+      <h1>式量計算機</h1>
+      <InputField onFieldChange={this.handleFieldChange}
+                  onFieldSubmit={this.handleSubmit}
+                  formula={this.state.formula}
+                  status={this.state.status}
+      />
+      <FormulaWeight fw={this.state.fw}/>
+      <table id="element-table">
+          <caption>元素情報</caption>
+          <thead>
+            <tr>
+              <th className="table-header">原子番号</th>
+              <th className="table-header">元素記号</th>
+              <th className="table-header">原子数</th>
+              <th className="table-header">原子量</th>
+              <th className="table-header">原子数比(%)</th>
+              <th className="table-header">質量比(%)</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderAllTableData()}
+          </tbody>
+        </table>
+    </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Layout />,
+  document.getElementById('app')
+);
---
+++ src/js/parseformula.js
@@ -0,0 +1,139 @@
+const pt = require('./element_table.js');
+
+// カッコのない化学式のチェック用
+const reFormulaChecker = /^(?:[A-Z][a-z]?[0-9]*)+$/;
+// カッコのない化学式のパース用
+const reFormula = /([A-Z][a-z]?)([0-9]*)/g;
+// 丸括弧を含む化学式のチェック・分割用
+const reRBracketSplitter = /(\((?:[A-Z][a-z]?[0-9]*)+\)[0-9]*)/;
+// 丸括弧を含む化学式のパース用
+const reRBracket = /\(((?:[A-Z][a-z]?[0-9]*)+)\)([0-9]*)/;
+// 角括弧を含む化学式のチェック・分割用
+const reRectBracketSplitter = /(\[[A-Za-z0-9\(\)]+\][0-9]*)/;
+// 角括弧を含む化学式のパース用
+const reRectBracket = /\[([A-Za-z0-9\(\)]+)\]([0-9]*)/;
+
+// 化学式を解析する
+// 戻り値はMap
+function parseFormula(formula) {
+  let ret = new Map();
+  let tmpRet = new Map();
+
+  if(formula === "" || formula === null || typeof formula === 'undefined') {
+    return null;
+  }
+
+  // 示性式対応のため'-''='を消去する（三重結合には対応しない）
+  for(let x of formula.replace(/\-|\=/g, '').split(reRectBracketSplitter)) {
+    let n = 1;
+
+    if(x.length === 0) {
+      continue;
+    }
+    else if(x[0] === '[') {
+      const tmp = x.match(reRectBracket);
+      tmpRet = parseFormulaBracket(tmp[1]);
+      n = (tmp[2] === "") ? 1 : Number(tmp[2]);
+    }
+    else{
+      tmpRet = parseFormulaBracket(x);
+    }
+
+    if(tmpRet === null) {
+      console.log('error: illegal formula.')
+      return null;
+    }
+
+    for(let [k, v] of tmpRet) {
+      if(ret.has(k)) {
+        ret.set(k, ret.get(k) + v * n);
+      }
+      else {
+        ret.set(k, v * n);
+      }
+    }
+    tmpRet.clear();
+  }
+
+  return ret;
+}
+
+// 角かっこのない化学式のパース
+function parseFormulaBracket(formula) {
+  let ret = new Map();
+  let tmpRet = new Map();
+
+  if(formula === "" || formula === null || typeof formula === 'undefined') {
+    return null;
+  }
+
+  for(let x of formula.split(reRBracketSplitter)) {
+    let n = 1;
+
+    if(x.length === 0) {
+      continue;
+    }
+    else if(x[0] === '(') {
+      const tmp = x.match(reRBracket);
+      tmpRet = parseFormulaCore(tmp[1]);
+      n = (tmp[2] === "") ? 1 : Number(tmp[2]);
+    }
+    else{
+      tmpRet = parseFormulaCore(x);
+    }
+
+    if(tmpRet === null) {
+      console.log('error: illegal formula.')
+      return null;
+    }
+
+    for(let [k, v] of tmpRet) {
+      if(ret.has(k)) {
+        ret.set(k, ret.get(k) + v * n);
+      }
+      else {
+        ret.set(k, v * n);
+      }
+    }
+    tmpRet.clear();
+  }
+
+  return ret;
+}
+
+// かっこの無い化学式のパース
+function parseFormulaCore(formula) {
+  let ret = new Map();
+  let n = 1;
+
+  if(formula === "" || formula === null || typeof formula === 'undefined') {
+    return null;
+  }
+
+  // 書式を満たしているかチェック
+  if(!reFormulaChecker.test(formula)){
+    return null;
+  }
+
+  // ([A-Z][a-z]?[0-9]*)
+  for(let x of formula.matchAll(reFormula)) {
+    const z = pt.periodicTable.symbolToNo(x[1]);
+    if(z === -1) {
+      console.log('error: illegal Symbol');
+      return null;
+    }
+
+    n = ((x[2].length === 0) ? 1 : Number(x[2]));
+
+    if(ret.has(x[1])) {
+      ret.set(x[1], ret.get(x[1]) + n);
+    }
+    else {
+      ret.set(x[1], n);
+    }
+  }
+
+  return ret;
+}
+
+module.exports = parseFormula;
